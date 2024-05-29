a379aa788be055534c1530d43f07c2855cb155d6
Isabelli <isa.pessoal78@gmail.com>
2024-05-28 21:28:55 -0300

    :sparkles:

--- utils/function.py
+++ utils/function.py
@@ -9,3 +9,41 @@ def limpar_texto(texto):
     texto_maiusculo = texto_sem_acento.upper()
 
     return texto_maiusculo
+
+
+class Exemplo:
+    @classmethod 
+    def etodo_de_classe(cls):
+        # comportamento modificado para operar na classe, não na instância.
+        pass
+
+    @staticmethod
+    def metodo_estatico():
+        #metodo que nbão depende da instância ou da classe.
+        pass
+
+class Motor:
+    def __init__(self, tipo):
+        self.tipo = tipo
+
+class Carro: 
+    def __init__(self):
+        self.motor = Motor(tipo="V8")
+
+    def ligar(self):
+        print("Carro ligado com um motor", self.motor.tipo)
+
+class Turma:
+    def __init__(self):
+        self.alunos = []
+
+    def adicionar_aluno(self, aluno):
+        self.alunos.append(aluno)
+
+    ## #Exemplo de uso ### 
+aluno1 = Aluno(nome = 'João')
+aluno2 = Aluno(nome = 'Maria')
+
+turma = Turma()
+turma.adicionar_aluno(aluno1)
+turma.adicionar_aluno(aluno2)
--- utils/helper.py
+++ utils/helper.py
@@ -1,3 +1,4 @@
 # __init__:Inicialização de objetos.
 # __str__ e __repr__:Representação de texto e debug.
 # __getitem__ e __setitem__:Manipulação de índices como em listas.
+
